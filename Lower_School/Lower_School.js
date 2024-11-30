889d4f8a6ba57c65ecd2f007dd8d830793fc796a
Justine <jcmagpoc7@gmail.com>
2024-11-30 23:32:54 +0800

    Justine

---
+++ Lower_School/Lower_School.js
@@ -0,0 +1,4 @@
+function openAdmissionForm() {
+    alert('Redirecting to Admission Form!');
+    window.location.href = 'https://www.lcctanauan.edu.ph'; // Replace with actual admission form link.
+}
